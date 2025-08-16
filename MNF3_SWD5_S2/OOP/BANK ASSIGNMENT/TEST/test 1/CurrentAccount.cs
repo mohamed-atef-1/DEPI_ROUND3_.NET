04dcb964ac017b680e2a00caf9be9a2bb56c8fdf
mohamed-atef-1 <mo7ammed.atef.38@gmail.com>
2025-08-16 23:01:45 +0300

    Add files via upload

---
+++ MNF3_SWD5_S2/OOP/BANK ASSIGNMENT/TEST/test 1/CurrentAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_1
+{
+    public class CurrentAccount : BankAccount
+    {
+            public decimal OverdraftLimit { get; set; }
+
+            public CurrentAccount(decimal overdraftLimit) 
+            {
+                OverdraftLimit = overdraftLimit;
+            }
+
+            public override void Withdraw(decimal amount)
+            {
+                if (amount <= 0) throw new Exception("Withdrawal amount must be positive.");
+                if (Balance + OverdraftLimit < amount) throw new Exception("Overdraft limit exceeded.");
+                Balance -= amount;
+                AddTransaction(new Transaction("Withdrawal", amount, $"Withdrawal from {AccountNumber} (Overdraft allowed)"));
+            }
+        
+    }
+}
---
+++ MNF3_SWD5_S2/OOP/BANK ASSIGNMENT/TEST/test 1/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_1
+{
+    
+    public class Transaction
+    {
+            public DateTime TransactionDate { get; set; }
+            public string Type { get; set; } // Deposit or  Withdrawal or Transfer
+            public decimal Amount { get; set; }
+            public string Description { get; set; }
+
+            public Transaction(string type, decimal amount, string description)   // constructor 
+            {
+                TransactionDate = DateTime.Now;
+                Type = type;
+                Amount = amount;
+                Description = description;
+            }
+       
+    }
+    
+
+}
+
